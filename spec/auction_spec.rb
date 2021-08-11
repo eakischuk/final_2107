0947c5206bc059a083335bb80e899d2bef42fc6d
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-11 09:20:37 -0600

    Add test for add_item method

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -14,4 +14,10 @@ RSpec.describe Auction do
     expect(@auction).to be_an(Auction)
     expect(@auction.items).to eq([])
   end
+
+  it 'can add items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.items).to eq([@item1, @item2])
+  end
 end
